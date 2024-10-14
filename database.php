92bacaa0c98b6666aaaee085ac7997e3d2616281
marcos-arduino <137747983+marcos-arduino@users.noreply.github.com>
2024-10-14 11:46:08 -0300

    Add files via upload

---
+++ database.php
@@ -0,0 +1,26 @@
+    <!-- Pyme de cosas de gimnasio 
+
+    No sabe cuanto puede hacer ni cuanto produce 
+    Quiere saber cuantas barras puede hacer con tantos kilos de hierro 
+    Usuario quiere hacer un kit cualquiera 
+    El propietario lo va a usar para saber cuando le entran las barras que kit puede hacer y cuantas barras o cuantas barras tiene 
+    Que el programa pueda guardar todos los datos que se le ingrese 
+    Barras pueden ser de 1,20m o 2m 
+    Que se vea en el celular 
+    Kit barras mariposas y discos 
+    ¿Cuando recibe el hierro?
+    Cuantas barras puede hacer con tantos kilos de hierro?
+    -Se tiene que ver en el celular
+    Idea programa que pueda hacer familias(kit) que el usuario pueda hacer el kit que quiera 
+    Insert Update Delete agregarlo como procedimiento almacenado para base de datos de couto
+
+    Calculo del hierro necesario por barra:
+-Barra olímpica masculina: Aproximadamente 22.22 kg de hierro.
+-Barra olímpica femenina: Aproximadamente 16.67 kg de hierro.
+-Barra de 1.20 m: Aproximadamente 10 kg de hierro.
+esto se debe a que se necesita un 10% mas de hierro por la perdida de material en la fundicion
+
+-->
+
+
+
